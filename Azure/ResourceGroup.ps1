1c67406ad51d3034c29afe18da5759b39a161480
artcolman <colman@drybridge.com>
2020-09-24 15:02:41 -0400

    update

--- Azure/ResourceGroup.ps1
+++ Azure/ResourceGroup.ps1
@@ -1,11 +1,16 @@
-using module InfoClasses
+using module D.InfoClasses
+Import-Module D.AzureFunctions
 
+#region Internal Variables
 $continueProcessing = $true
+$deploymentLocation = "eastus1"
+$resourceLocation = "eastus1"
 $subscriptionID = '0e4fd2e3-7f26-412d-a598-3901151357b1'
 $resourceGroupName = ""
+$tempateURI = ""
 # $tenantID = 'd1477d12-f77f-47a0-8b90-a8908fef66a2'
 
-Import-Module D.AzureFunctions
+#endregion
 
 ## Offline sign in - could this be a manual intervention?
 
@@ -21,16 +26,13 @@ if ($continueProcessing -eq $true) {
 
 if ($continueProcessing -eq $true) {
 
-    $rgGroup = New-ResourceGroup( -Name $resourceGroupName )
-
-    if($null -eq $rgGroup) {
-        $Script:continueProcessing = Create-ResourceGroup(-resourceGroupName $resourceGroupName)
-    }
-    else {
-        $Script:continueProcessing = $false  
-    }
-
+    $rgGroup = New-ResourceGroup `
+                -RGName $resourceGroupName `
+                -DeloymentLocation $deploymentLocation `
+                -TemplateURI $tempateURI `
+                -RGLocation $resourceLocation
 }
+
 #endregion
 
 ## Do other creates
