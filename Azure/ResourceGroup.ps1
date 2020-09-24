4c7cc1422e822ba35b6f0e1823fea5711431a6ad
artcolman <colman@drybridge.com>
2020-09-24 09:22:11 -0400

    Classes Implementation

--- Azure/ResourceGroup.ps1
+++ Azure/ResourceGroup.ps1
@@ -1,1 +1,41 @@
-import-module Drybridge.AzureFunctions
+using module InfoClasses
+
+$continueProcessing = $true
+$subscriptionID = '0e4fd2e3-7f26-412d-a598-3901151357b1'
+$resourceGroupName = ""
+# $tenantID = 'd1477d12-f77f-47a0-8b90-a8908fef66a2'
+
+Import-Module D.AzureFunctions
+
+## Offline sign in - could this be a manual intervention?
+
+#region Test connection
+
+if ($continueProcessing -eq $true) {
+    $Script:continueProcessing = Test-Subscription( -subscriptionID $subscriptionID )
+}
+
+#endregion
+
+#region Resource Group and Storage Account
+
+if ($continueProcessing -eq $true) {
+
+    $rgGroup = New-ResourceGroup( -Name $resourceGroupName )
+
+    if($null -eq $rgGroup) {
+        $Script:continueProcessing = Create-ResourceGroup(-resourceGroupName $resourceGroupName)
+    }
+    else {
+        $Script:continueProcessing = $false  
+    }
+
+}
+#endregion
+
+## Do other creates
+
+## Pause?
+
+## Clean-up
+
--- Test/Connect.ps1
+++ Test/Connect.ps1
@@ -1,1 +1,2 @@
+.4363
 Connect-AzAccount -Tenant 'd1477d12-f77f-47a0-8b90-a8908fef66a2' -SubscriptionId '0e4fd2e3-7f26-412d-a598-3901151357b1'
