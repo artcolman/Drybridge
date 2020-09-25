1f053f51e6bcdca314738554ea18a6a23c1acfa1
artcolman <colman@drybridge.com>
2020-09-25 17:05:46 -0400

    Update ResourceGroup.ps1

--- Azure/ResourceGroup.ps1
+++ Azure/ResourceGroup.ps1
@@ -3,15 +3,14 @@ Import-Module  D.AzureFunctions -Force
 
 $VerbosePreference = "Continue"
 
-
 #region Internal Variables
 $continueProcessing = $true
 $deploymentLocation = "eastus"
-$gitRepositoryURI = "https://github.com/artcolman/ARM-Templates/blob/master/"
+$gitRepositoryURI = "https://github.com/artcolman/ARM-Templates/blob/master/marker"
 $resourceGroupName = "rg-pshell-test-eastus-001"
 $resourceLocation = "eastus"
 $subscriptionID = '0e4fd2e3-7f26-412d-a598-3901151357b1'
-$templateName = "ResourceGroups/ResourceGroup.json"
+$templateName = "/ResourceGroups/ResourceGroup.json"
 #$tenantID = 'd1477d12-f77f-47a0-8b90-a8908fef66a2'
 
 #endregion
@@ -24,9 +23,6 @@ if ($continueProcessing -eq $true) {
 
     Write-Verbose 'Test-Subscription process initiated'
 
-    $context = Get-AzContext
-    $context | Select-Object -Property *
-    
     $Script:continueProcessing = Test-Subscription -subscriptionID $subscriptionID
 
     Write-Verbose 'Test-Subscription process finished'
@@ -39,17 +35,15 @@ if ($continueProcessing -eq $true) {
 
 if ($continueProcessing -eq $true) {
 
-    $tempateURI = Join-Path $gitRepositoryURI $templateName
+    $templateURI = $gitRepositoryURI.Replace("/marker", $templateName)
 
-    Write-Output $templateURI
+    Write-Verbose ( -join ("TemplateURI: ", $templateURI))
     
     $rgGroup = New-ResourceGroup `
                 -RGName $resourceGroupName `
-                -DeloymentLocation $deploymentLocation `
+                -DeploymentLocation $deploymentLocation `
                 -TemplateURI $tempateURI `
                 -RGLocation $resourceLocation
-
-    $rgGroup | Format-List * -Force
 
 }
 
